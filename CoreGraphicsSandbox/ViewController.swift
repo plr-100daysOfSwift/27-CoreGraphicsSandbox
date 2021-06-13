e373b6d41f428c7f6dd5168d1bea1856e2023a83
Paul Richardson <developer@photographerafoot.com>
2021-06-13 10:56:57 +1000

    CHG: inset rectangle to prevent clipping

--- CoreGraphicsSandbox/ViewController.swift
+++ CoreGraphicsSandbox/ViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
+
 	@IBOutlet var imageView: UIImageView!
 	var currentDrawType = 0
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		drawRectangle()
 	}
-	
+
 	@IBAction func redrawTapped(_ sender: Any) {
 		currentDrawType += 1
 		if currentDrawType > 5 {
@@ -31,44 +31,43 @@ class ViewController: UIViewController {
 			break
 		}
 	}
-	
+
 	func drawRectangle() {
 		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-		
+
 		let image = renderer.image { ctx in
 			let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
-			
+
 			ctx.cgContext.setFillColor(UIColor.red.cgColor)
 			ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
 			ctx.cgContext.setLineWidth(10)
-			
+
 			ctx.cgContext.addRect(rectangle)
 			ctx.cgContext.drawPath(using: .fillStroke)
 		}
-		
+
 		imageView.image = image
-		
+
 	}
-	
+
 	func drawCircle() {
 		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-		
+
 		let image = renderer.image { ctx in
-			let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
-			
+			let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+
 			ctx.cgContext.setFillColor(UIColor.red.cgColor)
 			ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
 			ctx.cgContext.setLineWidth(10)
-			
+
 			ctx.cgContext.addEllipse(in: rectangle)
 			ctx.cgContext.drawPath(using: .fillStroke)
 		}
-		
+
 		imageView.image = image
-		
+
 	}
-		
 	}
-	
+
 }
 
