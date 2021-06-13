1e758b14d035963b75cf470494611a523819acdd
Paul Richardson <developer@photographerafoot.com>
2021-06-13 10:53:53 +1000

    CHG: drawCircle()

--- CoreGraphicsSandbox/ViewController.swift
+++ CoreGraphicsSandbox/ViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	@IBOutlet var imageView: UIImageView!
 	var currentDrawType = 0
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		drawRectangle()
 	}
-
+	
 	@IBAction func redrawTapped(_ sender: Any) {
 		currentDrawType += 1
 		if currentDrawType > 5 {
@@ -25,27 +25,49 @@ class ViewController: UIViewController {
 		switch currentDrawType {
 		case 0:
 			drawRectangle()
+		case 1:
+			drawCircle()
 		default:
 			break
 		}
 	}
-
+	
 	func drawRectangle() {
 		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
-
+		
 		let image = renderer.image { ctx in
 			let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
-
+			
 			ctx.cgContext.setFillColor(UIColor.red.cgColor)
 			ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
 			ctx.cgContext.setLineWidth(10)
-
+			
 			ctx.cgContext.addRect(rectangle)
 			ctx.cgContext.drawPath(using: .fillStroke)
 		}
-
+		
 		imageView.image = image
-
+		
+	}
+	
+	func drawCircle() {
+		let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+		
+		let image = renderer.image { ctx in
+			let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+			
+			ctx.cgContext.setFillColor(UIColor.red.cgColor)
+			ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+			ctx.cgContext.setLineWidth(10)
+			
+			ctx.cgContext.addEllipse(in: rectangle)
+			ctx.cgContext.drawPath(using: .fillStroke)
+		}
+		
+		imageView.image = image
+		
+	}
+		
 	}
 	
 }
